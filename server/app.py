91a551dddd77570d748e37aa11bd772c8551ef3b
Ali1-coder <ablhur3@gmail.com>
2025-04-03 14:02:59 +0300

    All routes set

--- server/app.py
+++ server/app.py
@@ -14,13 +14,48 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+@app.route('/messages',methods=['GET'])
+def get_messages():
+    
+    messages=Message.query.order_by(Message.created_at.asc()).all()
+    messages_dict=[message.to_dict() for message in messages]
+    return jsonify(messages_dict),200
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+    
+
+@app.route('/messages',methods=['POST'])
+def post_message():
+    data=request.get_json()
+    new_message=Message(
+        body=data['body'],
+        username=data['username']
+    )
+
+    db.session.add(new_message)
+    db.session.commit()
+
+    return jsonify(new_message.to_dict()),201
+
+@app.route('/messages/<int:id>',methods=['PATCH'])
+def patch_message(id):
+    message=Message.query.filter(Message.id ==id).first()
+    data=request.get_json()
+    for attr, value in data.items():
+        if hasattr(message, attr): 
+            setattr(message, attr, value)
+    db.session.add(message)
+    db.session.commit()
+
+    return jsonify(message.to_dict()),200
+
+@app.route('/messages/<int:id>',methods=['DELETE'])
+def delete_message(id):
+    
+    message=Message.query.filter(Message.id ==id).first()
+    db.session.delete(message)
+    db.session.commit()
+
+    return make_response({'message': 'Message deleted'}, 200)
 
 if __name__ == '__main__':
     app.run(port=5555)
